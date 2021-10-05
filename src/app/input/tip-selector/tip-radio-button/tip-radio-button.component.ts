aa4d94d1280191071b9c52afcb6a60b24a18d57b
unknown <materiochi123@gmail.com>
2021-10-04 23:07:42 -0400

    create radio buttons using ngFor and radio component

--- src/app/input/tip-selector/tip-radio-button/tip-radio-button.component.ts
+++ src/app/input/tip-selector/tip-radio-button/tip-radio-button.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tip-radio-button',
-  template: `
-    <p>
-      tip-radio-button works!
-    </p>
-  `,
+  templateUrl: 'tip-radio-button.component.html',
   styles: [
   ]
 })
 export class TipRadioButtonComponent implements OnInit {
+  @Input() id: string = "";
+  @Input() name: string = "";
+  @Input() label: string = "";
 
   constructor() { }
 
