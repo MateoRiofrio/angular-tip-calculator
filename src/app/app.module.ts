fb4c3f2ffb54067b4f1ba2169d347621730a67d8
unknown <materiochi123@gmail.com>
2021-10-09 09:15:31 -0400

    update main page and app module with new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TipRadioButtonComponent } from './input/tip-selector/tip-radio-button/tip-radio-button.component';
-import { NumberTextFieldComponent } from './input/number-text-field/number-text-field.component';
 import { BoxContainerComponent } from './container/box-container/box-container.component';
 import { TextLabelComponent } from './output/text-label/text-label.component';
+import { LabelIconTextFieldComponent } from './input/text-field/label-icon-text-field/label-icon-text-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TipRadioButtonComponent,
-    NumberTextFieldComponent,
     BoxContainerComponent,
-    TextLabelComponent
+    TextLabelComponent,
+    LabelIconTextFieldComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
